68b9ab1be9f713794036891cb3cac2aa2a0246a6
AJ <joachim.liedtke@t-online.de>
2021-03-03 16:06:33 +0100

    feat(user.model.js): create user schema

---
+++ server/models/user.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('../db');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  categoriesToTrack: {
+    type: Array,
+    required: false
+  },
+  flights: {
+    type: Array,
+    required: false
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
