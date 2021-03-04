814041236a48063ba6fa68d094040172a6b2630d
AJ <joachim.liedtke@t-online.de>
2021-03-04 14:15:55 +0100

    fix(index.js): add cors handling

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
-// const indexRouter = require('./routes');
 const userRouter = require('./routes/user.router');
 const logbookRouter = require('./routes/logbook.router');
 
 const server = new Koa();
 const PORT = 3000;
 
+server.use(cors());
 server.use(bodyParser());
-// server.use(indexRouter.routes());
 server.use(userRouter.routes());
 server.use(logbookRouter.routes());
 
