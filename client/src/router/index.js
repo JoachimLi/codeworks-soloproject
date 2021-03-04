9d091d9708cde7138265f310c14582ba6736de96
AJ <joachim.liedtke@t-online.de>
2021-03-04 17:35:20 +0100

    feat(router/index.js): add router guard to profile page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
@@ -23,15 +24,26 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (profile.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
+    component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue'),
+    meta: {
+      requiresUser: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// Router guards to protect routes that require authentication
+router.beforeEach(async (to, from, next) => {
+  const requiresUser = to.matched.some(record => record.meta.requiresUser)
+
+  if (requiresUser && !store.state.user) next({ name: 'Login' })
+  else next();
 })
 
 export default router
