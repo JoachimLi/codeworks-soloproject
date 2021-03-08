049691ecc88ffb96a8a32a41d0a7cb09b821cfaf
AJ <joachim.liedtke@t-online.de>
2021-03-08 14:13:37 +0100

    feat(logbook.controller.js): add function to for calculating total times

--- server/controllers/logbook.controller.js
+++ server/controllers/logbook.controller.js
@@ -3,13 +3,14 @@ const { Flight, User } = require('../models');
 const createFlight = async (ctx) => {
   const flight = new Flight({...ctx.request.body});
 
-  // calculate total times
+  flight.blockTime = total(flight.offBlock, flight.onBlock);
+  flight.flightTime = total(flight.takeOff, flight.landing);
   
   try {
-    // const user = await User.findById(flight.userId);
-    // const newFlight = await flight.save();
-    // user.flights.push(newFlight._id.toString());    // newflight._id is of type ObjectId => needs to be of type String
-    // await user.save();                              // add flight to this users flights array
+    const user = await User.findById(flight.userId);
+    const newFlight = await flight.save();
+    user.flights.push(newFlight._id.toString());    // newflight._id is of type ObjectId => needs to be of type String
+    await user.save();                              // add flight to this users flights array
     ctx.status = 201;
     ctx.body = flight;
   } catch (error) {
@@ -35,5 +36,25 @@ const getFlights = async (ctx) => {
     ctx.body = error;
   }
 };
+
+// HELPER
+const total = (start, end) => {
+  let correction = 0;
+  const hrs = (end / 100 | 0) - (start / 100 | 0);
+
+  const mins = (() => {
+    const startMins = start % 100;
+    const endMins = end % 100;
+
+    if (startMins <= endMins) return endMins - startMins;
+    else {
+      correction = 1;
+      return endMins + 60 - startMins;
+    }
+  })();
+  
+  return `${hrs - correction}:${mins}`;
+};
+
 
 module.exports = { createFlight, getFlights };
