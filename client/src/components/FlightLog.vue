3d7d8b08f0d3d1aaa0016af88c4db76b846470b8
AJ <joachim.liedtke@t-online.de>
2021-03-08 16:32:37 +0100

    fix(flightlog.vue): improve styling

--- client/src/components/FlightLog.vue
+++ client/src/components/FlightLog.vue
@@ -2,64 +2,74 @@
   <div class="wrapper">
     <h3>flight log</h3>
     <div class="flight" v-for="flight in $store.state.flights" :key="flight._id">
-      <div class="container">
-        <div class="heading">date</div>
-        <div class="year">{{$moment(flight.date).format('YYYY')}}</div>
-        <div class="day">{{$moment(flight.date).format('DD MMM')}}</div>
+      <div class="flex">
+        <div class="container">
+          <div class="heading">date</div>
+          <div class="year">{{$moment(flight.date).format('YYYY')}}</div>
+          <div class="day">{{$moment(flight.date).format('DD MMM')}}</div>
+        </div>
+
+        <div class="container">
+          <div class="heading">aircraft</div>
+          <div class="registration">{{flight.aircraftRegistration}}</div>
+          <div class="type">{{flight.aircraftType}}</div>
+        </div>
+
+        <div class="container">
+          <div class="heading">route</div>
+          <div class="dep">{{flight.departure}}</div>
+          <div class="dest">{{flight.destination}}</div>
+          <div class="total">total:</div>
+        </div>
+
+        <div class="container">
+          <div class="heading">block</div>
+          <div class="date">{{flight.offBlock}}</div>
+          <div class="date">{{flight.onBlock}}</div>
+          <div class="total">{{flight.blockTime}}</div>
+        </div>
+
+        <div class="container">
+          <div class="heading">flight</div>
+          <div class="date">{{flight.takeOff}}</div>
+          <div class="date">{{flight.landing}}</div>
+          <div class="total">{{flight.flightTime}}</div>
+        </div>
       </div>
-      <div class="container">
-        <div class="heading">aircraft</div>
-        <div class="registration">{{flight.aircraftRegistration}}</div>
-        <div class="type">{{flight.aircraftType}}</div>
-      </div>
-      <div class="container">
-        <div class="heading">route</div>
-        <div class="dep">{{flight.departure}}</div>
-        <div class="dest">{{flight.destination}}</div>
-        <div class="total">total:</div>
-      </div>
-      <div class="container">
-        <div class="heading">block</div>
-        <div class="date">{{flight.offBlock}}</div>
-        <div class="date">{{flight.onBlock}}</div>
-        <div class="total">{{flight.blockTime}}</div>
-      </div>
-      <div class="container">
-        <div class="heading">flight</div>
-        <div class="date">{{flight.takeOff}}</div>
-        <div class="date">{{flight.landing}}</div>
-        <div class="total">{{flight.flightTime}}</div>
-      </div>
-      <div class="details" @click="toggleDetails(flight)">
-        <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 0 24 24" width="34px" fill="#cfcfcf">
-          <path d="M0 0h24v24H0V0z" fill="none"/>
-          <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z"/>
-        </svg>
+
+      <div class="details">
+        <div class="button" @click="toggleDetails(flight)">
+          <svg :class="{active: state.showDetails === flight._id}" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#cfcfcf">
+            <path d="M0 0h24v24H0V0z" fill="none"/>
+            <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
+          </svg>
+        </div>
+
+        <div class="categories" v-if="state.showDetails === flight._id">
+          <div v-for="category in flight.categories" :key="category.title">
+            {{category.title}}: {{category.timeLogged}}
+          </div>
+        </div>
+
       </div>
     </div>
-    <Modal v-if="state.showDetails" @toggleModal="toggleDetails" text="flight details">
-      <flight-details :flightDetails="state.flightDetails"/>
-    </Modal>
   </div>
 </template>
 
 <script>
 import { inject, reactive } from 'vue'
-import Modal from './Modal.vue'
-import FlightDetails from './FlightDetails.vue'
 export default {
-  components: { Modal, FlightDetails },
   name: 'FlightLog',
 
   setup () {
     const state = reactive({
-      showDetails: false, // show details modal
-      flightDetails: {}
+      showDetails: false
     })
 
     function toggleDetails (flight) {
-      state.flightDetails = flight
-      state.showDetails = !state.showDetails
+      console.log('flight._id', flight._id)
+      state.showDetails === flight._id ? state.showDetails = false : state.showDetails = flight._id
+      console.log('showDetails', state.showDetails)
     }
 
     const $moment = inject('$moment') // inject $moment, as provided in main.js
@@ -83,12 +93,17 @@ export default {
 
   .flight {
     display: flex;
+    flex-direction: column;
     justify-content: space-around;
     align-items: stretch;
     box-shadow: 0 0 5px #cfcfcf;
     border-radius: 5px;
     padding: 10px;
     margin-bottom: 20px;
+
+    .flex {
+      display: flex;
+    }
 
     .container {
       display: flex;
@@ -107,11 +122,33 @@ export default {
         border-top: 1px solid rgb(207, 207, 207);
       }
     }
+
     .details {
       display: flex;
       flex-direction: column;
-      justify-content: center;
-      cursor: pointer;
+      align-items: stretch;
+
+      .button {
+        cursor: pointer;
+
+        svg {
+          margin: -5px;
+          transition: transform 0.2s;
+
+          &.active {
+            transform: rotate(-180deg);
+          }
+        }
+      }
+
+      .categories {
+        display: flex;
+        flex-wrap: wrap;
+
+        div {
+          width: 20%;
+        }
+      }
     }
   }
 }
