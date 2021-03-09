4e07b469feb486c266b010e905b610b5adeb8e75
AJ <joachim.liedtke@t-online.de>
2021-03-09 14:15:20 +0100

    fix(logbook.apiservice.js): clean up code

--- client/src/ApiService/logbook.ApiService.js
+++ client/src/ApiService/logbook.ApiService.js
@@ -3,12 +3,10 @@ const axios = require('axios')
 const BASE_URL = 'http://localhost:3000/logbook'
 
 const getFlights = (userId) => {
-  // return axios.post(`${BASE_URL}/flights`, userId)
   return post(userId, 'flights')
 }
 
 const setFlight = (flight) => {
-  // return axios.post(`${BASE_URL}/new-flight`, flight)
   return post(flight, 'new-flight')
 }
 
