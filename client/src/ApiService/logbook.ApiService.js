97d7c6e2d79c239aff89afd1d6e6cabc07535a18
AJ <joachim.liedtke@t-online.de>
2021-03-09 14:14:31 +0100

    fix(logbook.apiservice.js): reduce repetition

--- client/src/ApiService/logbook.ApiService.js
+++ client/src/ApiService/logbook.ApiService.js
@@ -3,11 +3,17 @@ const axios = require('axios')
 const BASE_URL = 'http://localhost:3000/logbook'
 
 const getFlights = (userId) => {
-  return axios.post(`${BASE_URL}/flights`, userId)
+  // return axios.post(`${BASE_URL}/flights`, userId)
+  return post(userId, 'flights')
 }
 
 const setFlight = (flight) => {
-  return axios.post(`${BASE_URL}/new-flight`, flight)
+  // return axios.post(`${BASE_URL}/new-flight`, flight)
+  return post(flight, 'new-flight')
+}
+
+const post = (data, endpoint) => {
+  return axios.post(`${BASE_URL}/${endpoint}`, data)
 }
 
 module.exports = {
